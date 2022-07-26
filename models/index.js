f0cc64854a304b58c976561a237aba90c9ba148c
Hunter Padgett <gizmokabanjo@gmail.com>
2022-07-25 23:15:10 -0400

    set up models, working on routes

--- models/index.js
+++ models/index.js
@@ -4,13 +4,31 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category ✅
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
+// Categories have many Products ✅
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag) ✅
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  },
+  as: 'product_tags'
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag) ✅
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+  as: 'tag_products'
+});
 
 module.exports = {
   Product,
